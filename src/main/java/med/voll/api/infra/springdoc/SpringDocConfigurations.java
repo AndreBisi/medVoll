391b5d40467ebb83f0135d87fb2f0e84a8b794ad
AndreBisi <andregbisi@gmail.com>
2023-03-30 22:11:28 -0300

    atu

--- src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
+++ src/main/java/med/voll/api/infra/springdoc/SpringDocConfigurations.java
@@ -30,6 +30,7 @@ public class SpringDocConfigurations {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://voll.med/api/licenca")));
+
     }
 
 }
